c051eb2335ec2c5c0b3e6ef0adec769208269c2f
Marcus Tan <marcus.m.tan@gmail.com>
2016-01-23 18:14:43 -0500

    kamyar

--- src/flask/processing/processing.py
+++ src/flask/processing/processing.py
@@ -13,10 +13,10 @@ def get_semantics(file_name):
     """
     img = cv2.imread(file_name, 0)
     img = cv2.medianBlur(img, 5)
-    w, h, _ = img.shape
+    w, h = img.shape
     while 800 < w:
         img = cv2.pyrDown(img)
-        w, h, _ = img.shape
+        w, h = img.shape
     cimg = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR) 
     
 
